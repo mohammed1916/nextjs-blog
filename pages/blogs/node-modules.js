cee585704496277fdf14bc4d828a673eaa8c970b
Mohammed <arrahmaan9901@gmail.com>
2023-05-30 19:11:11 +0530

    add blog: node-modules

---
+++ pages/blogs/node-modules.js
@@ -0,0 +1,14 @@
+import Head from "next/head";
+import Link from "next/link";
+import Layout from "../../components/layout";
+
+export default function DarkMode() {
+    return (
+        <Layout>
+            <Head>
+                <title>Node.js Modules</title>
+            </Head>
+            <h1>Node.js Modules</h1>
+        </Layout>
+    );
+}
