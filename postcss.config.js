880c61d6acb05e0b31e77ce4a57c868f7e0fb694
Mohammed <arrahmaan9901@gmail.com>
2023-05-31 07:52:22 +0530

    config error, should contain either globals.css

--- postcss.config.js
+++ postcss.config.js
@@ -6,8 +6,4 @@ module.exports = {
         tailwindcss: {},
         autoprefixer: {}
     },
-    content: [
-        './pages/**/*.{js,ts,jsx,tsx}',
-        './components/**/*.{js,ts,jsx,tsx}',
-    ]
 }
