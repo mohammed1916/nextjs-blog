6bb996da8a158d9be26d19da34132d5d189d994d
Mohammed <arrahmaan9901@gmail.com>
2023-05-31 15:46:09 +0530

    getStaticProps-show blog titles in home page

--- pages/index.js
+++ pages/index.js
@@ -3,8 +3,9 @@ import styles from '../styles/Home.module.css';
 import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link';
 import Layout, { siteTitle } from '../components/layout';
+import { getSortedPostsData } from '../utils/posts';
 
-export default function Home() {
+export default function Home({ allPostsData }) {
   return (
     <Layout home>
       <Head>
@@ -56,7 +57,31 @@ export default function Home() {
           </a>
         </div>
       </main>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
+      </section>
 
     </Layout>
   );
 }
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    }
+  }
+}
+
